c8e96627cdffec5cc0a98396f26df494f56f86ff
Chase McCarthy <chase@code0100fun.com>
2016-07-11 22:27:41 -0400

    Add mock email to index route

---
+++ app/routes/index.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return [{
+      from: 'recruiter@startup.com',
+      to: 'dev@me.com',
+      subject: 'Opportunity',
+      body: 'Dear {{firstName}}, ...',
+      sentAt: new Date()
+    }];
+  }
+});
