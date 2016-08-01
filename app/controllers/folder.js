96e8f2922fc92c4cffb00059170ab125e255a8b6
Jonathan Jackson <jonathan.jackson1@me.com>
2016-08-01 14:19:44 -0400

    User sees trash item in navigation
    [finishes #125756841]
    https://www.pivotaltracker.com/story/show/125756841
    
    Added Mail Lookup Service
       This provides us with a place to put the fixture logic that will be
       easy to change when we move to Ember Data and a real backend

---
+++ app/controllers/folder.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+const { Controller } = Ember;
+
+export default Controller.extend({
+  mailLookup: Ember.inject.service(),
+  inboxCount: Ember.computed.alias('mailLookup.inboxCount'),
+  trashCount: Ember.computed.alias('mailLookup.trashCount')
+});
---
+++ app/services/mail-lookup.js
@@ -0,0 +1,47 @@
+import Ember from 'ember';
+import moment from 'moment';
+
+const fixtures = [{
+  id: 1,
+  from: 'recruiter@startup.com',
+  to: 'dev@me.com',
+  subject: 'Opportunity',
+  body: 'Dear {{firstName}}, ...',
+  sentAt: moment(new Date()).subtract(2, 'minute').toDate(),
+  readDate: null,
+  trashedDate: null
+}, {
+  id: 2,
+  from: 'spam@twitter.com',
+  to: 'dev@me.com',
+  subject: 'You may like these ads',
+  body: 'Some irrelevant ads',
+  sentAt: moment(new Date()).subtract(1, 'day').toDate(),
+  readDate: null,
+  trashedDate: null
+}];
+
+const { get, set } = Ember;
+
+export default Ember.Service.extend({
+  lookupFilters: {
+    inbox: i => !get(i, 'trashedDate'),
+    trash: i => get(i, 'trashedDate'),
+  },
+  retrieve(folderName) {
+    return new Ember.RSVP.Promise((resolve) => {
+      const lookupFilters = get(this, 'lookupFilters');
+
+      for(var fn in lookupFilters) {
+        const filter = lookupFilters[fn];
+        const result = fixtures.filter(filter);
+
+        set(this, `${fn}Count`, result.length);
+        set(this, fn, result);
+      }
+
+      resolve(get(this, folderName));
+    });
+  }
+
+});
