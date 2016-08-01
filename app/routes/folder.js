4ed96384c2a45e056473acd5f04b644f94b4f774
Jonathan Jackson <jonathan.jackson1@me.com>
2016-08-01 14:19:44 -0400

    User sees trash item in navigation
    [finishes #125756841]
    https://www.pivotaltracker.com/story/show/125756841
    
    Added Mail Lookup Service
       This provides us with a place to put the fixture logic that will be
       easy to change when we move to Ember Data and a real backend

--- app/routes/folder.js
+++ app/routes/folder.js
@@ -1,32 +1,10 @@
 import Ember from 'ember';
-import moment from 'moment';
-
-const fixtures = [{
-  id: 1,
-  from: 'recruiter@startup.com',
-  to: 'dev@me.com',
-  subject: 'Opportunity',
-  body: 'Dear {{firstName}}, ...',
-  sentAt: moment(new Date()).subtract(2, 'minute').toDate(),
-  readDate: null,
-  trashedDate: null
-}, {
-  id: 2,
-  from: 'spam@twitter.com',
-  to: 'dev@me.com',
-  subject: 'You may like these ads',
-  body: 'Some irrelevant ads',
-  sentAt: moment(new Date()).subtract(1, 'day').toDate(),
-  readDate: null,
-  trashedDate: null
-}];
 
 const { get } = Ember;
 
 export default Ember.Route.extend({
-  model() {
-    return fixtures.filter(function(item) {
-      return !get(item, 'trashedDate');
-    });
+  mailLookup: Ember.inject.service(),
+  model(params) {
+    return get(this, 'mailLookup').retrieve(params.folderName);
   }
 });
